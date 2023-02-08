d160affdd3f1613de4b349ed4fc8132a8ae181c5
ijbd <ijbd@umich.edu>
2023-02-08 14:56:36 -0500

    Add summary autogen script.

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -1,47 +1,66 @@
 import os
-from numpy import mean, std
+from numpy import mean, std, percentile
 import pandas as pd
 import  argparse
 from sklearn.metrics import mean_absolute_percentage_error as mape, mean_squared_error as mse
 
-BAL_AUTH_CODES = ["MISO", "SWPP", "PJM", "ERCOT", "CAISO", "PGE", "SOCO"]
+BAL_AUTH_CODES = ["MISO", "SWPP", "PJM", "ERCO", "CISO", "PGE", "SOCO"]
 
 THIS_DIR = os.path.dirname(__file__)
 OUTPUT_DIR = os.path.join(THIS_DIR, "..", "OUTPUT")
+SUMMARY_FILE = os.path.join(THIS_DIR, "..", "summary.csv")
+
+LABEL_SUFFIX = {"WEIGHTED" : "2020-pop",
+				"UNWEIGHTED" : "unweighted",
+				"CENTROID" : "centroid"}
 
 def mbe(y_true: list, y_pred: list) -> float:
 	""" Mean Bias Error """	
-	return np.mean(y_pred) - np.mean(y_true)
+	return mean(y_pred) - mean(y_true)
 
 def load_data(data_file: str) -> pd.DataFrame:
-	return pd.read_csv(data_file)
+	return pd.read_csv(data_file, index_col=0, parse_dates=True)
 
 def main():
 
-	summary = pd.DataFrame(index=BAL_AUTH_CODES)
+	summary = pd.DataFrame()
 
 	for bal_auth in BAL_AUTH_CODES:
 
-		# filenames
-		temp_file = dict()
-		temp_file["WEIGHTED"] = os.path.join(OUTPUT_DIR, f"{bal_auth}-temperature-2020-pop.csv")
-		temp_file["UNWEIGHTED"] = os.path.join(OUTPUT_DIR, f"{bal_auth}-temperature-unweighted.csv")
-		temp_file["CENTROID"] = os.path.join(OUTPUT_DIR, f"{bal_auth}-temperature-centroid.csv")
-
 		# load data
 		temp = dict()
-		temp["WEIGHTED"] = load_data(temp_file["WEIGHTED"])
-		temp["UNWEIGHTED"] = load_data(temp_file["UNWEIGHTED"])
-		temp["CENTROID"] = load_data(temp_file["CENTROID"])
 
-		for label in ["WEIGHTED", "UNWEIGHTED", "CENTROID"]:
-			summary.loc[bal_auth, f"{label} MEAN"] = mean(temp[label])
-			summary.loc[bal_auth, f"{label} STD"] = std(temp[label])
+		for label in LABEL_SUFFIX:
+			# filename
+			temp_file = os.path.join(OUTPUT_DIR, f"{bal_auth}-temperature-{LABEL_SUFFIX[label]}.csv")
+			temp[label] = load_data(temp_file)
+
+		# extreme hours
+		pct_25, pct_75 = percentile(temp["WEIGHTED"].values, [25, 75])
+		top_25_pct = temp["WEIGHTED"].index[temp["WEIGHTED"]["Temperature (K)"] >= pct_75]
+		bottom_25_pct = temp["WEIGHTED"].index[temp["WEIGHTED"]["Temperature (K)"] <= pct_25]
+
+		for label in LABEL_SUFFIX:
+			temp[f"{label}-T25"] = temp[label].loc[top_25_pct]
+			temp[f"{label}-B25"] = temp[label].loc[bottom_25_pct]
+
+		# gather stats
+		bal_auth_summary = pd.Series(name=bal_auth, dtype=float)
+
+		# individual
+		for tag in ["", "-T25", "-B25"]:
+			for label in ["WEIGHTED", "UNWEIGHTED", "CENTROID"]:
+				bal_auth_summary.loc[f"{label}{tag} MEAN"] = mean(temp[f"{label}{tag}"].values)
+				bal_auth_summary.loc[f"{label}{tag} STD"] = std(temp[f"{label}{tag}"].values)
 		
-		for label in ["UNWEIGHTED", "CENTROID"]
-			summary.loc[bal_auth, f"WEIGHTED-{label} MAPE"] = mape(temp["WEIGHTED"])
-			summary.loc[bal_auth, f"WEIGHTED-{label} MSE"] =
-			summary.loc[bal_auth, f"WEIGHTED-{label} MBE"] = 
+			# comparative
+			for label in ["UNWEIGHTED", "CENTROID"]:
+				bal_auth_summary.loc[f"WEIGHTED-{label}{tag} MAPE"] = mape(temp[f"WEIGHTED{tag}"].values, temp[f"{label}{tag}"].values)
+				bal_auth_summary.loc[f"WEIGHTED-{label}{tag} MSE"] = mse(temp[f"WEIGHTED{tag}"].values, temp[f"{label}{tag}"].values)
+				bal_auth_summary.loc[f"WEIGHTED-{label}{tag} MBE"] = mbe(temp[f"WEIGHTED{tag}"].values, temp[f"{label}{tag}"].values)
+
+		summary = pd.concat((summary, bal_auth_summary),axis=1)
+	summary.T.to_csv(SUMMARY_FILE)
 
 if __name__ == "__main__":
 	main()
